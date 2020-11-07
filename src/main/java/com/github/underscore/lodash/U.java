e8604933b8388232c2f99a00952a44214d8ada57
Valentyn Kolesnikov <javadev75@gmail.com>
2020-11-07 07:31:56 +0200

    Sync with underscore-java.

--- src/main/java/com/github/underscore/lodash/U.java
+++ src/main/java/com/github/underscore/lodash/U.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright 2015-2019 Valentyn Kolesnikov
+ * Copyright 2015-2020 Valentyn Kolesnikov
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -2458,6 +2458,11 @@ public class U<T> extends com.github.underscore.U<T> {
         }
 
         public Builder add(final String key, final ArrayBuilder builder) {
+            data.put(key, builder.build());
+            return this;
+        }
+
+        public Builder add(final String key, final Builder builder) {
             data.put(key, builder.build());
             return this;
         }
